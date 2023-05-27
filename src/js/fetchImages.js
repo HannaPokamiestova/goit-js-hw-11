ffb7da12081b227cf568fcf90dcd003f75f2c0ca
Hanna Pokamiestova <105422641+HannaPokamiestova@users.noreply.github.com>
2023-05-27 08:54:05 +0200

    Update fetchImages

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 const URL = 'https://pixabay.com/api/';
 
-export async function fetchImages(query) {
+export async function fetchImages(query, page, perPage) {
   const key = '36660815-e6e20e8460ba8bd16a81efb15';
-}
 
-try {
-  const response = await axios.get(
-    `${URL}? key=${key}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
+  try {
+    const response = await axios.get(
+      `${URL}? key=${key}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}& per_page=${perPage}`
+    );
 
-  return response.data;
-} catch (error) {
-  console.log(error);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
